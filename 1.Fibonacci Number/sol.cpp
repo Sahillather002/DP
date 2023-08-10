34e9fd6cb74383d1f177d01fc11478053336faa7
Sahil Lather <sahillather002@gmail.com>
2023-08-10 16:47:34 +0530

    leetcode updated sol.cpp

--- 1.Fibonacci Number/sol.cpp
+++ 1.Fibonacci Number/sol.cpp
@@ -16,3 +16,22 @@ int main()
         cout<<fib(n,dp)<<endl;
         return 0;
 }
+
+
+// leetcode one
+
+class Solution {
+public:
+    int solve(int n,vector<int>& dp){
+        if(n==1 || n==0){
+            return 1;
+        }
+        if(dp[n]!=-1)return dp[n];
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
