0372d86e5ebbc44c95b44d611f7a806e44d94965
Sahil Lather <sahillather002@gmail.com>
2023-08-10 16:57:38 +0530

    Create dp.cpp

---
+++ 6. Coin Change Problem/dp.cpp
@@ -0,0 +1,51 @@
+// unbounded knapsack 
+// a little diff from bounded knapsack
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long int count(int coins[], int n, int sum) {
+        long long int dp[n+1][sum+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i==0)dp[i][j]=0;
+                if(j==0)dp[i][j]=1;
+            }
+        }
+        long long int ans=0;
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(coins[i-1]<=j){
+                    dp[i][j]=dp[i-1][j] + dp[i][j-coins[i-1]];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int sum, N;
+        cin >> sum >> N;
+        int coins[N];
+        for (int i = 0; i < N; i++) cin >> coins[i];
+        Solution ob;
+        cout << ob.count(coins, N, sum) << endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
